5d8f7ce6879528ce5354335d48dc1b2a5e773b18
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-06 23:10:45 +0530

    Update PassOne.java

--- PassOne.java
+++ PassOne.java
@@ -6,3 +6,16 @@ import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.io.PrintWriter; 
 import java.util.HashMap;
+
+class Symbol
+{
+	String sname; 
+	int addr;
+	int length;
+
+	Symbol(String name) 
+	{
+		this.sname = name;
+	}
+	
+	
