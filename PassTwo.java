5454a0cb104a73e183bc1223627444c85c324bd8
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-09 23:00:31 +0530

    Update PassTwo.java

--- PassTwo.java
+++ PassTwo.java
@@ -1,2 +1,13 @@
 //Author: Shubham Bhagwansing Rajput
 //Java
+
+	public static void main(String[] args) throws Exception {
+			File f = new File("Pass1_output.txt"); 
+			BufferedReader br = new BufferedReader(new FileReader (f));
+
+			PassTwo obj = new PassTwo();
+			obj.initTables();
+			obj.passTwo(br);
+	}
+
+}
